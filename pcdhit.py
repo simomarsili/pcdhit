ebd7bc4d3ca299ecbee81726b724465004e4ac4f
simomarsili <simomarsili@gmail.com>
2019-02-12 18:41:54 +0100

    fix mail

--- pcdhit.py
+++ pcdhit.py
@@ -6,7 +6,7 @@ project_name = 'pcdhit'
 __version__ = pkg_resources.require(project_name)[0].version
 __copyright__ = 'Copyright (C) 2019 Simone Marsili'
 __license__ = 'BSD 3 clause'
-__author__ = 'Simone Marsili (simo.marsili@gmail.com)'
+__author__ = 'Simone Marsili <simo.marsili@gmail.com>'
 __all__ = ['filter']
 
 
