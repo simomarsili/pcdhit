fc0a78b84fba4703614c7ff0a0bfdc88aee91da8
simomarsili <simo.marsili@gmail.com>
2019-02-26 13:01:47 +0100

    fix tests (lilbio args)

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -18,8 +18,8 @@ def test_pcdhit():
     import os
     import lilbio
     import pcdhit
-    from lilbio.funcs import only_hmm
+    from lilbio.funcs import uppercase_only
     source = os.path.join(tests_dir(), alignment_file)
-    records = lilbio.parse(source, 'stockholm', func=only_hmm)
+    records = lilbio.parse(source, 'stockholm', func=uppercase_only)
     filtered_records = pcdhit.filter(records, 0.7)
     assert len(list(filtered_records)) == 61
